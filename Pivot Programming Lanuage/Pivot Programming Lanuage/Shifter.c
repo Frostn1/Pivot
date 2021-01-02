adaa8f4ddc15a5192e390bb6360c017c1a49a736
Sean Dahan <aui.svi@gmail.com>
2021-01-02 23:31:08 +0200

    From progress
    A bit more progress on the from writing to file

--- Pivot Programming Lanuage/Pivot Programming Lanuage/Shifter.c
+++ Pivot Programming Lanuage/Pivot Programming Lanuage/Shifter.c
@@ -21,7 +21,33 @@ void Shift(Parser* par)
 				Throw("From Error", "Missing '}' on ", par->tokens_t[i]->row, par->tokens_t[i + 2]->col);
 			}
 			//LoadCode(Slice(par->tokens_t[i + 2]->self,0,strlen(par->tokens_t[i + 2]->self)-1)
+			char* buffer = (char*)malloc(sizeof(char) * (strlen(Slice(par->tokens_t[i + 2]->self, 0, strlen(par->tokens_t[i + 2]->self))) + 5));
+			buffer[0] = 'L';
+			buffer[1] = 'O';
+			buffer[2] = 'A';
+			buffer[3] = 'D';
+			buffer[4] = ' ';
+			int j = 0;
+			for (j = 0; j < strlen(par->tokens_t[i + 2]->self-1)-1; j++)
+			{
+				buffer[j + 5] = Slice(par->tokens_t[i + 2]->self, 0, strlen(par->tokens_t[i + 2]->self) - 1)[j];
+			}
+			
+			//strcat(buffer, Slice(par->tokens_t[i + 2]->self, 0, strlen(par->tokens_t[i + 2]->self)-1));
+
+			fwrite(buffer,1,strlen(buffer)*sizeof(char),par->file);
 			printf("FROM\n");
+			i += 2;
 		}
+		else if (!strcmp(par->tokens_t[i]->self, "func"))
+		{
+			printf("FUNC\n");
+			if (par->tokens_t[i + 1]->self[strlen(par->tokens_t[i + 1]->self) - 1] != '(' && par->tokens_t[i + 2]->self[0] != '(')
+			{
+				Throw("Func Error", "Missing '(' on ", par->tokens_t[i + 1]->row, par->tokens_t[i + 1]->col);
+			}
+
+		}
+	
 	}
 }
--- Pivot Programming Lanuage/Pivot Programming Lanuage/Shifter.h
+++ Pivot Programming Lanuage/Pivot Programming Lanuage/Shifter.h
@@ -1,6 +1,6 @@
 #ifndef SHIFTER_H
 #define SHIFTER_H
 #include "Parser.h"
-
+#include "Utils.h"
 void Shift(Parser* par);
 #endif // !SHIFTER_H
