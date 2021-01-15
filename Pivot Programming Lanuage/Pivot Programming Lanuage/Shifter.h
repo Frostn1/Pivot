7369549c9c96028ec4ec929ed5f18bab237b78c5
Sean Dahan <aui.svi@gmail.com>
2021-01-15 03:37:04 +0200

    Func and variable starting
    Functions are pretty much finished while having the signature all figured out the ending point is the only think that remains, variable are at a really good starting point and I got a lot done

--- Pivot Programming Lanuage/Pivot Programming Lanuage/Shifter.h
+++ Pivot Programming Lanuage/Pivot Programming Lanuage/Shifter.h
@@ -1,6 +1,9 @@
+#define _CRT_NONSTDC_NO_DEPRECATE
 #ifndef SHIFTER_H
 #define SHIFTER_H
 #include "Parser.h"
 #include "Utils.h"
+#include "ConInPost.h"
+#include "CalcPost.h"
 void Shift(Parser* par);
 #endif // !SHIFTER_H
