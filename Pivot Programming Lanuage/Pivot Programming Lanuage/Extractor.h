c0ee34690108d451afb80dd11f72a0b7200356b5
Sean Dahan <aui.svi@gmail.com>
2021-02-22 22:42:44 +0200

    Complete Rebuild & Restart
    Completed teh restart and cleaning and storaging all pre files, and ready for rebuild

--- Pivot Programming Lanuage/Pivot Programming Lanuage/Extractor.h
+++
@@ -1,9 +0,0 @@
-#ifndef EXTRACTOR_H
-#define EXTRACTOR_H
-#include "Utils.h"
-#include "Lexer.h"
-Lexer* Exctract(char* filePath);
-
-#endif // !EXTRACTOR_H
-
-
--- Pivot Programming Lanuage/Pivot Programming Lanuage/Source1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "Tokenizer.h"
-#include "Extractor.h"
-int main()
-{
-	//Process("D:\\Programming\\Projects\\Pivot\\Pivot Programming Lanuage\\Pivot Programming Lanuage\\main.pi");
-	Exctract("D:\\Programming\\Projects\\Pivot\\Pivot Programming Lanuage\\Pivot Programming Lanuage\\main.pi");
-	return 0;
-}
