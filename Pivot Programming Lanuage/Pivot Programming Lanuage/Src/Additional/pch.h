15facbe3dbbab2ec932ab5af8ab56f531eee9668
Sean Dahan <aui.svi@gmail.com>
2021-02-23 00:52:54 +0200

    Rebrand Pivot
    Rebuilding Pivot from the ground, and got some early dev and settings started out, as Version controller and token defenitions

---
+++ Pivot Programming Lanuage/Pivot Programming Lanuage/Src/Additional/pch.h
@@ -0,0 +1,7 @@
+#include "../Lexer/Lexer.h"
+
+
+#define VERSION_PIV "{ Pre }"
+#define Major_V 0
+#define General_V 0
+#define Minimal_V 1
---
+++ Pivot Programming Lanuage/Pivot Programming Lanuage/Src/Lexer/Lexer.h
@@ -0,0 +1,11 @@
+#ifndef LEXER_T
+#define LEXER_T
+
+typedef struct PI_LEXER {
+	int lineCounter;
+	int columnCounter;
+
+
+}lexer;
+
+#endif LEXER_T
