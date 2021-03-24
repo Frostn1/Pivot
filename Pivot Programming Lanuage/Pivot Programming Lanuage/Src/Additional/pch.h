c955c7fa1802c888689cf9b48eb6b0468e6cc6bb
Sean Dahan <aui.svi@gmail.com>
2021-03-24 19:22:21 +0200

    Removal of files

--- Pivot Programming Lanuage/Pivot Programming Lanuage/Src/Additional/pch.h
+++
@@ -1,9 +0,0 @@
-#include "../Lexer/Lexer.h"
-
-
-#define VERSION_PIV "{ Pre }"
-#define Major_V 0
-#define General_V 0
-#define Minimal_V 1
-// Current Version 0.0.1
-// "D:\\Programming\\Projects\\Pivot\\Pivot Programming Lanuage\\Pivot Programming Lanuage\\Testing\\Version 0.0.1\\Intial Lexer Testing\\code.piv"
