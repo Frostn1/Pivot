58edb9552978a3c15a012724f7e3909e66054ee2
Sean Dahan <aui.svi@gmail.com>
2021-05-15 22:50:19 +0300

    Project Restart

--- Code Examples/code/code.c
+++
@@ -1,7 +0,0 @@
-int main() {
-    int num = 20;
-    num = 10;
-    int num1 = 20;
-    num1 += num;
-    return 0;
-}
--- tree-sitter-pivot/highlights/highlights.scm
+++
@@ -1,6 +0,0 @@
-; highlights.scm
-
-"func" @keyword
-"return" @keyword
-(string_literal) @string
-(integer_literal) @int
